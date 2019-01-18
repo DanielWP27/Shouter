479960aff0213236818a858c366d9aba9120572d
Daniel <daniel.keighley@wpengine.com>
2019-01-18 11:54:35 +0000

    Add custom admin for Shout & User. Add shout_list method to User model.

--- shoutsite/shouts/models.py
+++ shoutsite/shouts/models.py
@@ -10,9 +10,16 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+    def list_shouts(self):
+        #verbose
+        #shout_list = ", ".join([Shout.objects.filter(user=self.id)])
+        shout_list = ", ".join([str(shout.id) for shout in self.shouts.all()])
+        return shout_list
+
 
 class Shout(models.Model):
     shout_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date shouted')
     likes = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='shouts')
+    #get rid of null & blank at some point
